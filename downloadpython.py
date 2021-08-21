afe1190e0800337c361b0f626fdc904c2f8fcdba
thejasveerathal <32195119+thejasveerathal@users.noreply.github.com>
2021-08-21 08:27:53 -0700

    python update 2.0

--- downloadpython.py
+++ downloadpython.py
@@ -1,10 +1,23 @@
-import os
 import requests
+import os
+from pathlib import Path
 
-print ("Hello World")
-path = os.getcwd()
-print(path)
-print (os.listdir())
-print(type(path))
-response = requests.get ('http://172.18.0.3:8081/repository/micro-integrator/MP-FavouriteRoute/MP-FavouriteRoute-CAR/1.0.0/MP-FavouriteRoute-CAR-1.0.0.zip')
-print(response)
+print('Download Starting...')
+ 
+url = 'http://172.18.0.3:8081/repository/micro-integrator/MP-FavouriteRoute/MP-FavouriteRoute-CAR/1.0.0/MP-FavouriteRoute-CAR-1.0.0.zip'
+p = Path(url)
+
+artifact = p.name
+
+
+req = requests.get(url, stream=True)
+ 
+ 
+with open(p.name,'wb') as output_file:
+    for chunk in req.iter_content(chunk_size=1025):
+        if chunk:
+            output_file.write(chunk)
+
+print('Download Completed...')            
+ 
+os.rename(artifact,artifact.replace("zip","car")) 
