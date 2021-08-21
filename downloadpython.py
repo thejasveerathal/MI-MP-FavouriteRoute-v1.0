03c81866f037b3f35534cfbc0bf795a5e7104fde
thejasveerathal <32195119+thejasveerathal@users.noreply.github.com>
2021-08-21 07:15:40 -0700

    script update 2.0

--- downloadpython.py
+++ downloadpython.py
@@ -1,3 +1,10 @@
+import os
 import requests
 
-response = requests.get('http://172.18.0.3:8081/repository/micro-integrator/MP-FavouriteRoute/MP-FavouriteRoute-CAR/1.0.0/MP-FavouriteRoute-CAR-1.0.0.zip')
+print ("Hello World")
+path = os.getcwd()
+print(path)
+print (os.listdir())
+print(type(path))
+response = requests.get ('http://172.18.0.3:8081/repository/micro-integrator/MP-FavouriteRoute/MP-FavouriteRoute-CAR/1.0.0/MP-FavouriteRoute-CAR-1.0.0.zip')
+print(response)
